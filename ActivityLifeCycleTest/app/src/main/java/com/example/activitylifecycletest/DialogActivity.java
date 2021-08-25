099757cb0588707e8ef6d11670560f04d1b6d217
小赫赫 <1404119786@qq.com>
2021-08-25 19:46:50 +0800

    Activity 生命周期学习

---
+++ ActivityLifeCycleTest/app/src/main/java/com/example/activitylifecycletest/DialogActivity.java
@@ -0,0 +1,15 @@
+package com.example.activitylifecycletest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DialogActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_layout);
+    }
+}
